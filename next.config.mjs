d02c77ed1a7d1dbc70dd3362a189f32502c95c62
TONKLA POKAEW (CW) <tonkla.pokaew@wdc.com>
2024-05-07 11:42:58 +0700

    eqwe

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.externals.push('mongoose');
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
